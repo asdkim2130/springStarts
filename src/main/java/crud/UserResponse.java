a5dad7bc73ff3978fc971a44f925f7d7e9508e9d
HYOJIN <asdkim2130@gmail.com>
2024-12-16 17:38:33 +0900

    map DB

--- src/main/java/crud/UserResponse.java
+++ src/main/java/crud/UserResponse.java
@@ -22,6 +22,8 @@ public class UserResponse {
     public String getName() {
         return name;
     }
-    //오브젝터의 getter를 사용하지 않더라도 만들어야 함
+    //오브젝터의 getter를 사용하지 않더라도 만들어야 함. JASON 데이터로 변환이 되어야 하기 때문에(Spring에서 JASON으로 변환할 때 필요함)
 
+
+    //DTO 데이터 운반 오브젝트. 데이터 운반 말고는 다른 기능이 없음
 }
